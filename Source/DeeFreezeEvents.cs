ba0823b55937694409a7b92e4a4b36e0b680ce64
ScottPaladin <Scott.Paladin@gmail.com>
2014-09-05 09:37:42 -0500

    Refactored OnVesselRecovered Code
    Pulled the code to find all the variables of a type/module out into its
    own section so I can reference it for OnVesselDestroy

---
+++ Source/DeeFreezeEvents.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using DeepFreezer;
+
+
+namespace DeepFreezer
+{
+    class DeepFreezeEvents
+    {
+        public static DeepFreezeEvents instance = new DeepFreezeEvents();
+        public bool eventAdded;
+
+        public DeepFreezeEvents()
+        {
+            eventAdded = false;
+        }
+
+        public void DeepFreezeEventAdd()
+        {
+            GameEvents.OnVesselRecoveryRequested.Add(this.OnVesselRecoveryRequested);
+            GameEvents.onVesselRecoveryProcessing.Add(this.onVesselRecoveryProcessing);
+            GameEvents.onVesselRecovered.Add(this.onVesselRecovered);
+            eventAdded = true;
+
+        }
+
+
+        public void OnVesselRecoveryRequested(Vessel vessel)
+        {
+
+            if (vessel.FindPartModulesImplementing<DeepFreezer>().Count > 0)
+            {
+                foreach (DeepFreezer freezer in vessel.FindPartModulesImplementing<DeepFreezer>())
+                {
+                    freezer.part.CrewCapacity = freezer.StoredCrew.Count;
+                    foreach (var crewmember in freezer.StoredCrew)
+                    {
+                        foreach (ProtoCrewMember kerbal in HighLogic.CurrentGame.CrewRoster.Crew)
+                        {
+                            if (kerbal.name == crewmember)
+                                freezer.part.AddCrewmember(kerbal);
+                            Debug.Log("Crew Added" + kerbal.name);
+                        }
+
+                    }
+                }
+            }
+        }
+        public void onVesselRecoveryProcessing(ProtoVessel vessel, MissionRecoveryDialog d, float f)
+        {
+            Debug.Log("OnVesselRecoveryProcessing");
+        }
+        //public void onVesselRecovered(ProtoVessel vessel) Old Version of onVesselRecovered Known to work
+        //{
+        //    List<ProtoPartSnapshot> partList = vessel.protoPartSnapshots;
+        //    foreach (ProtoPartSnapshot a in partList)
+        //    {
+        //        List<ProtoPartModuleSnapshot> modules = a.modules;
+        //        foreach (ProtoPartModuleSnapshot module in modules)
+        //        {
+
+        //            if (module.moduleName == "DeepFreezer")
+        //            {
+        //                ConfigNode node = module.moduleValues;
+        //                string FrozenCrew = node.GetValue("FrozenCrew");
+        //                ThawFrozenCrew(FrozenCrew);
+        //            }
+        //        }
+        //    }
+        //}
+
+        public void onVesselRecovered(ProtoVessel vessel) //RefactoredOnVessel Recovered to use the new FindProtoModuleVariable, should work for more than just this one job now.
+        {
+                        List<string> ProtoModuleVariables = FindProtoModuleVariable(vessel, "DeepFreezer", "FrozenCrew");
+                        foreach (string FrozenCrew in ProtoModuleVariables)
+                        {
+                            ThawFrozenCrew(FrozenCrew);
+                        }
+          
+        }
+        public List<String> FindProtoModuleVariable(ProtoVessel vessel, String m, String s)
+        {
+            List<ProtoPartSnapshot> partList = vessel.protoPartSnapshots;
+            List<String> result = null;
+            foreach (ProtoPartSnapshot a in partList)
+            {
+                List<ProtoPartModuleSnapshot> modules = a.modules;
+                foreach (ProtoPartModuleSnapshot module in modules)
+                {
+
+                    if (module.moduleName == m)
+                    {
+                        ConfigNode node = module.moduleValues;
+                        string tempresult = node.GetValue(s);
+                        result.Add(tempresult);
+                    }
+                }
+            }
+            return result;
+        }
+        public void ThawFrozenCrew(String FrozenCrew)
+        {
+            List<String> StoredCrew = FrozenCrew.Split(',').ToList();
+            foreach (string frozenkerbal in StoredCrew)
+            {
+                foreach (ProtoCrewMember kerbal in HighLogic.CurrentGame.CrewRoster.Crew) //There's probably a more efficient way to find Protocrewmember from the CrewRoster
+                {
+                    if (kerbal.name == frozenkerbal)
+                    {
+                        kerbal.rosterStatus = ProtoCrewMember.RosterStatus.Available;
+                        ScreenMessages.PostScreenMessage(kerbal.name + " was found in and thawed out.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                    }
+                }
+            }
+        }
+
+        //    //foreach (ProtoPartModuleSnapshot module in a)
+        //    //{
+
+        //    //}
+        //    ProtoPartModuleSnapshot freezer = a.modules.Find(n => n.moduleName == "DeepFreezer");
+        //    if (freezer != null)
+        //    {
+        //        if (freezer.moduleValues.GetValue("FrozenCrew") == null)
+        //        {
+        //            Debug.Log("Freezer foud but FrozenCrew value is null");
+        //        }
+        //    }
+
+
+
+
+
+    }
+}
--- Source/DeepFreezer.cs
+++ Source/DeepFreezer.cs
@@ -3,9 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using DeepFreezer;
+
 
 namespace DeepFreezer
 {
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    class DeepFreeze : MonoBehaviour
+    {
+
+        public void Start()
+        {
+            if (!DeepFreezeEvents.instance.eventAdded)
+                Debug.Log(DeepFreezeEvents.instance.eventAdded);
+            {
+                DeepFreezeEvents.instance.DeepFreezeEventAdd();
+                Debug.Log("Start called");
+            }
+        }
+
+
+    }
     public class DeepFreezer : PartModule
     {
         private float lastUpdate = 0.0f;
@@ -15,11 +33,17 @@ namespace DeepFreezer
         [KSPField(isPersistant = true, guiActive = true, guiName = "Frozen Crew")]
         public string FrozenCrew;
 
-        [KSPField(isPersistant = true, guiActive = true, guiName = "Freezer Size")]
+        [KSPField(isPersistant = true, guiActive = false, guiName = "Freezer Size")]
         public int FreezerSize;
+
+        [KSPField(isPersistant = true, guiActive = true, guiName = "Total Frozen Kerbals")]
+        public int TotalFrozen;
 
         [KSPField(isPersistant = true, guiActive = true, guiName = "Freezer Space")]
         public int FreezerSpace;
+
+        [KSPField(isPersistant = true)]
+        public bool IsCrewableWhenFull;
 
         public List<string> StoredCrew;
 
@@ -27,39 +51,58 @@ namespace DeepFreezer
         {
             if ((Time.time - lastUpdate) > updatetnterval)
             {
+                base.OnInactive();
                 lastUpdate = Time.time;
                 UpdateEvents();
                 FrozenCrew = String.Join(",", StoredCrew.ToArray());
             }
         }
+
         public override void OnLoad(ConfigNode node)
         {
             FrozenCrew = node.GetValue("FrozenCrew");
-            Debug.Log(FrozenCrew);
-            Int32.TryParse(node.GetValue("FreezerSpace"), out FreezerSize);
+            //Debug.Log(FrozenCrew);
+            Int32.TryParse(node.GetValue("FreezerSize"), out FreezerSize);
+            //Debug.Log(FreezerSize + " " + FreezerSpace);
+            IsCrewableWhenFull = Convert.ToBoolean(node.GetValue("IsCrewableWhenFull"));
             LoadFrozenCrew();
         }
         public override void OnSave(ConfigNode node)
         {
             node.SetValue("FrozenCrew", FrozenCrew);
         }
-
-        private void LoadFrozenCrew()
+        public override void OnInactive()
         {
-            Debug.Log(FrozenCrew);
+            Debug.Log("OnInactive" + FrozenCrew);
+            part.CrewCapacity = StoredCrew.Count;
+            foreach (var crewmember in StoredCrew)
+            {
+
+                foreach (ProtoCrewMember kerbal in HighLogic.CurrentGame.CrewRoster.Crew)
+                {
+                    if (kerbal.name == crewmember)
+                        part.AddCrewmember(kerbal);
+                }
+
+            }
+            base.OnInactive();
+        }
+
+        private void LoadFrozenCrew() //The FrozenCrew variable is a string, we need it split into a list.
+        {
             if (!string.IsNullOrEmpty(FrozenCrew))
             {
                 StoredCrew = FrozenCrew.Split(',').ToList();
-                foreach (string value in StoredCrew)
-                    Debug.Log("Stored " + value);
             }
         }
 
         private void UpdateEvents()
         {
+            UpdateCounts();
             Events.Clear();
             if (StoredCrew.Count < FreezerSize)
             {
+                part.CrewCapacity = 1;
                 foreach (var CrewMember in part.protoModuleCrew)
                 {
                     Events.Add(new BaseEvent(Events, "Freeze " + CrewMember.name, () =>
@@ -70,16 +113,19 @@ namespace DeepFreezer
                         }
                         if ((FreezerSize - StoredCrew.Count) > 0)
                         {
-                            if (requireResource(vessel, "Electric Charge", 100, true) == true);
+                            if (!requireResource(vessel, "Glykerol", 10, true))
                             {
-                            FreezeKerbal(CrewMember);
+                                ScreenMessages.PostScreenMessage("Insufficient Glykerol to freeze kerbal.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
+                                return;
                             }
+                            FreezeKerbal(CrewMember);
+
                         }
 
                     }, new KSPEvent { guiName = "Freeze " + CrewMember.name, guiActive = true }));
                 }
             }
-            if (part.protoModuleCrew.Count < part.CrewCapacity)
+            if ((part.protoModuleCrew.Count < part.CrewCapacity) || part.CrewCapacity <= 0)
                 foreach (var frozenkerbal in StoredCrew)
                 {
                     Events.Add(new BaseEvent(Events, "Thaw" + frozenkerbal, () =>
@@ -88,6 +134,10 @@ namespace DeepFreezer
 
                     }, new KSPEvent { guiName = "Thaw " + frozenkerbal, guiActive = true }));
                 }
+            if (StoredCrew.Count >= FreezerSize && IsCrewableWhenFull == true)
+            {
+                part.CrewCapacity = 0;
+            }
 
         }
         public void FreezeKerbal(ProtoCrewMember CrewMember)
@@ -97,8 +147,8 @@ namespace DeepFreezer
             CrewMember.rosterStatus = ProtoCrewMember.RosterStatus.Dead;
             ScreenMessages.PostScreenMessage(CrewMember.name + " frozen", 5.0f, ScreenMessageStyle.UPPER_CENTER);
             FrozenCrew = String.Join(",", StoredCrew.ToArray());
-            Debug.Log("FrozenCrew =" + FrozenCrew);
-            FreezerSpace = (FreezerSize - StoredCrew.Count);
+            //Debug.Log("FrozenCrew =" + FrozenCrew);
+            UpdateCounts();
         }
         public void ThawKerbal(String frozenkerbal)
         {
@@ -106,12 +156,14 @@ namespace DeepFreezer
             {
                 if (kerbal.name == frozenkerbal)
                 {
+                    part.CrewCapacity = 1;
                     part.AddCrewmember(kerbal);
                     ScreenMessages.PostScreenMessage(kerbal.name + " thawed out.", 5.0f, ScreenMessageStyle.UPPER_CENTER);
-                    Debug.Log(StoredCrew.Remove(kerbal.name));
+                    //Debug.Log(StoredCrew.Remove(kerbal.name));
                     FrozenCrew = String.Join(",", StoredCrew.ToArray());
-                    Debug.Log("FrozenCrew =" + FrozenCrew);
-                    FreezerSpace = (FreezerSize - StoredCrew.Count);
+                    //Debug.Log("FrozenCrew =" + FrozenCrew);
+                    UpdateCounts();
+
                 }
             }
         }
@@ -150,9 +202,11 @@ namespace DeepFreezer
             }
             return true;
         }
-
+        public void UpdateCounts()
+        {
+            FreezerSpace = (FreezerSize - StoredCrew.Count);
+            TotalFrozen = StoredCrew.Count;
+        }
     }
 
-
 }
-
