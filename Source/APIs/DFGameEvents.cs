d2423bf01abaa482e979479bb02ea8d70251afb5
Jamie Leighton <jamieleighton@bigpond.com>
2016-12-08 17:57:49 +1100

    Fix DFGameEvent initialisation.

--- Source/APIs/DFGameEvents.cs
+++ Source/APIs/DFGameEvents.cs
@@ -11,26 +11,26 @@ namespace DF
         /// Fires when DeepFreeze Completes the Freezing process on a Kerbal.
         /// Part is the DeepFreeze Freezer Part and ProtoCrewMember is the Kerbal.
         /// </summary>
-        public static EventData<Part, ProtoCrewMember> onKerbalFrozen = new EventData<Part, ProtoCrewMember>("onKerbalFrozen");
+        public static EventData<Part, ProtoCrewMember> onKerbalFrozen;// = new EventData<Part, ProtoCrewMember>("onKerbalFrozen");
         /// <summary>
         /// Fires when DeepFreeze Completes the Thawing process on a Kerbal.
         /// Part is the DeepFreeze Freezer Part and ProtoCrewMember is the Kerbal.
         /// </summary>
-        public static EventData<Part, ProtoCrewMember> onKerbalThaw = new EventData<Part, ProtoCrewMember>("onKerbalThaw");
+        public static EventData<Part, ProtoCrewMember> onKerbalThaw;// = new EventData<Part, ProtoCrewMember>("onKerbalThaw");
         /// <summary>
         /// Fires when DeepFreeze sets a Kerbal to Comatose Status.
         /// Part is the DeepFreeze Freezer Part and ProtoCrewMember is the Kerbal.
         /// </summary>
-        public static EventData<Part, ProtoCrewMember> onKerbalSetComatose = new EventData<Part, ProtoCrewMember>("onKerbalSetComatose");
+        public static EventData<Part, ProtoCrewMember> onKerbalSetComatose;// = new EventData<Part, ProtoCrewMember>("onKerbalSetComatose");
         /// <summary>
         /// Fires when DeepFreeze Unsets a Kerbal from Comatose Status.
         /// Part is the DeepFreeze Freezer Part and ProtoCrewMember is the Kerbal.
         /// </summary>
-        public static EventData<Part, ProtoCrewMember> onKerbalUnSetComatose = new EventData<Part, ProtoCrewMember>("onKerbalUnSetComatose");
+        public static EventData<Part, ProtoCrewMember> onKerbalUnSetComatose;// = new EventData<Part, ProtoCrewMember>("onKerbalUnSetComatose");
         /// <summary>
         /// Fires when DeepFreeze has to Kill a Frozen Kerbal.
         /// </summary>
-        public static EventData<ProtoCrewMember> onFrozenKerbalDied = new EventData<ProtoCrewMember>("onFrozenKerbalDied");
+        public static EventData<ProtoCrewMember> onFrozenKerbalDied;// = new EventData<ProtoCrewMember>("onFrozenKerbalDied");
 
     }
 }
